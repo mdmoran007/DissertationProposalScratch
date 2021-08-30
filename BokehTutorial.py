dd07c83e411d9edb4b4605616837f5f096a901e9
mdmoran007 <mdmoran007@hotmail.com>
2021-08-30 15:47:11 -0500

    All nine tutorial sections completed.

--- BokehTutorial.py
+++ BokehTutorial.py
@@ -1,4 +1,4 @@
-# Bokeh Tutorial from:
+# Bokeh Tutorial starting from:
 #   - https://docs.bokeh.org/en/latest/docs/first_steps/first_steps_1.html
 #
 # I am making some changes and consolidating some sections, but it will give me a good basis to learn.
@@ -578,3 +578,305 @@ p.line(x, y, line_width=2)
 # show the results
 bokeh.io.output_file('BokehTutorial-4N.html')
 show(p)
+
+# First Steps 5: Vectorizing glyph properties -------------------------------------
+# In the previous first steps guide, you customized various aspects of your plot by adding and changing attributes.
+# In this section, you will use vectors of data to influence aspects of your plot and its elements.
+
+# Vectorizing colors
+# So far, you have assigned specific colors to a glyph by using properties such as fill_color.
+# To change colors depending on values in a variable,
+# pass a variable containing color information to the fill_color attribute:
+
+# generate some data (1-10 for x, random values for y)
+x = list(range(0, 26))
+y = random.sample(range(0, 100), 26)
+
+# generate list of rgb hex colors in relation to y
+colors = ["#%02x%02x%02x" % (255, int(round(value * 255 / 100)), 255) for value in y]
+
+# create new plot
+p = figure(
+    title="Vectorized colors example",
+    sizing_mode="stretch_width",
+    max_width=500,
+    plot_height=250,
+)
+
+# add circle and line renderers
+line = p.line(x, y, line_color="blue", line_width=1)
+circle = p.circle(x, y, fill_color=colors, line_color="blue", size=15)
+
+# show the results
+bokeh.io.output_file('BokehTutorial-5A.html')
+show(p)
+
+# Vectorizing Colors and Sizes
+# Here, apply the same principle to the radius
+
+import numpy as np
+
+# generate some data
+N = 1000
+x = np.random.random(size=N) * 100
+y = np.random.random(size=N) * 100
+
+# generate radii and colors based on data
+radii = y / 100 * 2
+colors = ["#%02x%02x%02x" % (255, int(round(value * 255 / 100)), 255) for value in y]
+
+# create a new plot with a specific size
+p = figure(
+    title="Vectorized colors and radii example",
+    sizing_mode="stretch_width",
+    max_width=500,
+    plot_height=250,
+)
+
+# add circle renderer
+p.circle(
+    x,
+    y,
+    radius=radii,
+    fill_color=colors,
+    fill_alpha=0.6,
+    line_color="lightgrey",
+)
+
+# show the results
+bokeh.io.output_file('BokehTutorial-5B.html')
+show(p)
+
+# Color mapping with palettes
+# Bokeh comes with dozens of pre-defined color palettes that you can use to map colors to your data.Th
+# is includes palettes from Brewer, D3, or Matplotlib.
+#
+# First, use the linear_cmap() function to create a color map for your data.
+# The required attributes for this function are:
+#   - field_name: the data sequence to map colors to
+#   - palette: the palette to use
+#   - low: the lowest value to map a color to
+#   - high: the highest value to map a color to
+# Then assign your color mapper to the color parameter of your renderer:
+
+from bokeh.palettes import Turbo256
+from bokeh.plotting import figure
+from bokeh.transform import linear_cmap
+
+# generate data
+x = list(range(-32, 33))
+y = [i**2 for i in x]
+
+# create linear color mapper
+mapper = linear_cmap(field_name="y", palette=Turbo256, low=min(y), high=max(y))
+
+# create plot
+p = figure(plot_width=500, plot_height=250)
+
+# create circle renderer with color mapper
+p.circle(x, y, color=mapper, size=10)
+
+bokeh.io.output_file('BokehTutorial-5C.html')
+show(p)
+
+# First steps 6: Combining plots -----------------------------------------------
+# In the previous first steps guides, you created individual plots.
+# In this section, you will combine several plots into different kinds of layouts.
+
+# Creating rows, columns, and grids
+# The easiest way to combine individual plots is to assign them to rows or columns.
+
+from bokeh.layouts import row
+from bokeh.plotting import figure, show
+
+# prepare some data
+x = list(range(11))
+y0 = x
+y1 = [10 - i for i in x]
+y2 = [abs(i - 5) for i in x]
+
+# create three plots with one renderer each
+s1 = figure(plot_width=250, plot_height=250, background_fill_color="#fafafa")
+s1.circle(x, y0, size=12, color="#53777a", alpha=0.8)
+
+s2 = figure(plot_width=250, plot_height=250, background_fill_color="#fafafa")
+s2.triangle(x, y1, size=12, color="#c02942", alpha=0.8)
+
+s3 = figure(plot_width=250, plot_height=250, background_fill_color="#fafafa")
+s3.square(x, y2, size=12, color="#d95b43", alpha=0.8)
+
+# put the results in a row and show
+# To display several plots in a vertical column layout, use the column() function instead.
+# A more flexible way to arrange elements in Bokeh is to use the gridplot() function.
+bokeh.io.output_file('BokehTutorial-6A.html')
+show(row(s1, s2, s3))
+
+# You can also make it more responive to the browser.
+# create three plots with one renderer each
+s1 = figure(plot_width=250, plot_height=250, background_fill_color="#fafafa")
+s1.circle(x, y0, size=12, color="#53777a", alpha=0.8)
+
+s2 = figure(plot_width=250, plot_height=250, background_fill_color="#fafafa")
+s2.triangle(x, y1, size=12, color="#c02942", alpha=0.8)
+
+s3 = figure(plot_width=250, plot_height=250, background_fill_color="#fafafa")
+s3.square(x, y2, size=12, color="#d95b43", alpha=0.8)
+
+# put the results in a row that automatically adjusts
+# to the browser window's width
+bokeh.io.output_file('BokehTutorial-6B.html')
+show(row(children=[s1, s2, s3], sizing_mode="scale_width"))
+
+# First steps 7: Displaying and exporting ----------------------------------------------------
+# This section will work with formatting, exporting, and saving the visualizations.
+# THe ability to do it in a png requires a driver that the M1 does not appear to have yet, but I can export.
+
+from bokeh.plotting import figure, output_file, save, show
+
+# prepare some data
+x = [1, 2, 3, 4, 5]
+y = [4, 5, 5, 7, 2]
+
+# set output to static HTML file
+output_file(filename="BokehTutorial-7A.html", title="Your Title Here")
+
+# create a new plot with a specific size
+p = figure(sizing_mode="stretch_width", max_width=500, plot_height=250)
+
+# add a circle renderer
+circle = p.circle(x, y, fill_color="red", size=15)
+
+# save the results to a file
+save(p) # Without showing
+# Show (and save) the results
+show(p) # Will also work directly in a Jupyter notebook.
+
+# This section had to be customized to check hardware since it does not work on the native M1 architecture.
+# This requires selenium be installed to work. It also looks like it needs a chromedriver.
+import platform
+from bokeh.io import export_png
+from bokeh.plotting import figure
+
+if platform.machine() == 'arm64':
+    print('The right drivers don\'t exist to run this on native MacOS arm64')
+else:
+    # prepare some data
+    x = [1, 2, 3, 4, 5]
+    y = [4, 5, 5, 7, 2]
+
+    # create a new plot with fixed dimensions
+    p = figure(plot_width=350, plot_height=250)
+
+    # add a circle renderer
+    circle = p.circle(x, y, fill_color="red", size=15)
+
+    # save the results to a file
+    export_png(p, filename='BokenTutorial-7B.png')
+
+# First steps 8: Providing and filtering data --------------------------------
+# In this section, we will use various sources and structures to import and filter data.
+
+from bokeh.models import ColumnDataSource
+
+# create dict as basis for ColumnDataSource
+data = {'x_values': [1, 2, 3, 4, 5],
+        'y_values': [6, 7, 2, 3, 6]}
+
+# create ColumnDataSource based on dict
+source = ColumnDataSource(data=data)
+# You could also convert a pandas dataframe with a command like:
+# source = ColumnDataSource(df)
+
+# create a plot and renderer with ColumnDataSource data
+p = figure()
+p.circle(x='x_values', y='y_values', source=source)
+
+bokeh.io.output_file(filename="BokehTutorial-8A.html")
+show(p)
+
+# Filtering Data
+from bokeh.layouts import gridplot
+from bokeh.models import CDSView, ColumnDataSource, IndexFilter
+from bokeh.plotting import figure, show
+
+# create ColumnDataSource from a dict
+source = ColumnDataSource(data=dict(x=[1, 2, 3, 4, 5], y=[1, 2, 3, 4, 5]))
+
+# create a view using an IndexFilter with the index positions [0, 2, 4]
+view = CDSView(source=source, filters=[IndexFilter([0, 2, 4])])
+
+# setup tools
+tools = ["box_select", "hover", "reset"]
+
+# create a first plot with all data in the ColumnDataSource
+p = figure(plot_height=300, plot_width=300, tools=tools)
+p.circle(x="x", y="y", size=10, hover_color="red", source=source)
+
+# create a second plot with a subset of ColumnDataSource, based on view
+# There are other filters. This just filters out the data points at that index position.
+p_filtered = figure(plot_height=300, plot_width=300, tools=tools)
+p_filtered.circle(x="x", y="y", size=10, hover_color="red", source=source, view=view)
+
+# show both plots next to each other in a gridplot layout
+bokeh.io.output_file(filename="BokehTutorial-8B.html")
+show(gridplot([[p, p_filtered]]))
+
+# First steps 9: Using widgets -----------------------------------------------
+# To make a more interactive html viz, you can use widgets.
+# This can also be done with a bokeh server.
+
+from bokeh.layouts import layout
+from bokeh.models import Div, RangeSlider, Spinner
+from bokeh.plotting import figure, show
+
+# prepare some data
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+y = [4, 5, 5, 7, 2, 6, 4, 9, 1, 3]
+
+# create plot with circle glyphs
+p = figure(x_range=(1, 9), plot_width=500, plot_height=250)
+points = p.circle(x=x, y=y, size=30, fill_color="#21a7df")
+
+# set up textarea (div)
+div = Div(
+    text="""
+          <p>Select the circle's size using this control element:</p>
+          """,
+    width=200,
+    height=30,
+)
+
+# set up spinner
+spinner = Spinner(
+    title="Circle size",
+    low=0,
+    high=60,
+    step=5,
+    value=points.glyph.size,
+    width=200,
+)
+spinner.js_link("value", points.glyph, "size")
+
+# set up RangeSlider
+range_slider = RangeSlider(
+    title="Adjust x-axis range",
+    start=0,
+    end=10,
+    step=1,
+    value=(p.x_range.start, p.x_range.end),
+)
+range_slider.js_link("value", p.x_range, "start", attr_selector=0)
+range_slider.js_link("value", p.x_range, "end", attr_selector=1)
+
+# create layout
+layout = layout(
+    [
+        [div, spinner],
+        [range_slider],
+        [p],
+    ]
+)
+
+# show result
+bokeh.io.output_file('BokehTutorial-9A.html')
+show(layout)
