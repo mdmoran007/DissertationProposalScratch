d952a8336b514269b3b4171ec0723f33abd612d9
mdmoran007 <mdmoran007@hotmail.com>
2022-04-16 08:19:07 -0500

    CSR-ESGDataBuild1.1.py - not sure what changes were made there, but I'll save it nonetheless. Like the other 1.1 files, it's purpose was to expand the research from the Industrial Sector to the Economy.

--- CSR-ESGDataBuild1.1.py
+++ CSR-ESGDataBuild1.1.py
@@ -1,10 +1,3 @@
-# I think I need to add the list of tickers and then the offset of their fiscal year. I might re-work those structures.
-# I think the easier way at this scale is to use one of the existing master files as an input to build the right structures.
-
-# Need to check directories and file locations.
-# Need to see if i really need the founded data...I think I have that already.
-
-
 # This file will build the records to be added into the MasterStakeholderSheet for the ESG data.
 # It will be keyed from Ticker and Fiscal Year
 # It will contain one average value from each of the five files per ticker per year.
@@ -14,45 +7,51 @@
 import pandas as pd
 
 # Declare variables ------------------------------------------------------------------------------
-TickerList = ['BA', 'CAT', 'CMI', 'CSX', 'CTAS', 'DE', 'DOV', 'EFX', 'EMR', 'FDX', 'GD',
-              'GE', 'GWW', 'HON', 'ITW', 'LMT', 'LUV', 'MAS', 'MMM', 'NOC', 'NSC', 'PCAR',
-              'PH', 'RHI', 'ROK', 'RTX', 'SNA', 'SWK', 'TXT', 'UNP', 'UPS', 'WM']
+
+# TickerList = ['BA', 'CAT', 'CMI', 'CSX', 'CTAS', 'DE', 'DOV', 'EFX', 'EMR', 'FDX', 'GD',
+#              'GE', 'GWW', 'HON', 'ITW', 'LMT', 'LUV', 'MAS', 'MMM', 'NOC', 'NSC', 'PCAR',
+#              'PH', 'RHI', 'ROK', 'RTX', 'SNA', 'SWK', 'TXT', 'UNP', 'UPS', 'WM']
 
 # Matches to Ticker List above.
-OffsetList = [0, 0, 0, 0, 5, -2, 0, 0, -3, 5, 0,
-              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-              -6, 0, -3, 0, 0, 0, 0, 0, 0, 0]
+# OffsetList = [0, 0, 0, 0, 5, -2, 0, 0, -3, 5, 0,
+#              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+#             -6, 0, -3, 0, 0, 0, 0, 0, 0, 0]
 
-StartYear = 2009
-EndYear = 2020
+StartYear = 2009  # Actual first year of data
+EndYear = 2019  # 1 is added to this later in the code...this is the actual end year.
+
 
 # Will need to calculate these values for each record.
 # Will start the outfile with this row
-Header = ['Ticker', 'Fiscal_Year', 'T-FY', 'Age', 'CSR_ESG', 'CSR_Env', 'CSR_Com', 'CSR_Emp', 'CSR_Gov']
+# Header = ['Ticker', 'Fiscal_Year', 'Key', 'CSR_ESG', 'CSR_Env', 'CSR_Com', 'CSR_Emp', 'CSR_Gov']
 
-# Where to find the input files. Will have to work through all five at once I suppose?
-BaseDir = '/Users/moranmarkd/OneDrive/Academics/Illinois/Dissertation/ESGData'
-InFileList = ['CSR-ESG-1.csv', 'CSR-Env-1.csv', 'CSR-Com-1.csv', 'CSR-Emp-1.csv', 'CSR-Gov-1.csv']
+out_file = '/Users/moranmarkd/OneDrive/Academics/Illinois/Dissertation/Paper1.1/Out/esg-v1.csv'
+mstr_in_file = '/Users/moranmarkd/OneDrive/Academics/Illinois/Dissertation/MasterData/MasterTickerData.csv'
 
-OutFile = '/Users/moranmarkd/OneDrive/Academics/Illinois/Dissertation/Paper1.1/Out/esg-v1.csv'
-
-#Might want to add this too, not sure.
+# Might want to add this too, not sure.
 # Just seems good to have as another potentially moderating variable.
 # Could use this to add company age at each point.
-FoundedList = [1916, 1925, 1919, 1980, 1929, 1837, 1955, 1899, 1890, 1971, 1899,
-               1892, 1927, 1906, 1912, 1995, 1967, 1929, 1902, 1930, 1881, 1905,
-               1917, 1948, 1903, 1922, 1920, 1843, 1923, 1862, 1907, 1968]
 
 # Build dictionary from two lists
+mstr_df = pd.read_csv(mstr_in_file)
+print (mstr_df)
+print (mstr_df['Ticker'])
+print (mstr_df['FYEndOffset'])
+
+TickerList = mstr_df['Ticker'].tolist()
+OffsetList = mstr_df['FYEndOffset'].tolist()
+
+print (len(TickerList), TickerList)
+print (len(OffsetList), OffsetList)
+
 TickerOffsetDic = {TickerList[i]: OffsetList[i] for i in range(len(TickerList))}
-TickerFoundedDic = {TickerList[i]: FoundedList[i] for i in range(len(TickerList))}
-
 print(TickerOffsetDic)
-print(TickerFoundedDic)
 
-# Begin function definitions ----------------------------------------------------------------------------
 
-def fisc_list_fxn(fy, om): # --------------------------------------------------------------------------------
+# Begin function definitions --------------------------------------------------------------------------------
+
+
+def fisc_list_fxn(fy, om):  # --------------------------------------------------------------------------------
     # fy = fiscal year, om = offset month
     # Used to find which months are in a fiscal year
     # Returns a list of 12 items in the format YYYY.M
@@ -127,7 +126,7 @@ env_out_list = [['Key', "CSR_Env_Mean"]]
 com_out_list = [['Key', "CSR_Com_Mean"]]
 emp_out_list = [['Key', "CSR_Emp_Mean"]]
 gov_out_list = [['Key', "CSR_Gov_Mean"]]
-age_out_list = [['Key', 'Age']]
+#age_out_list = [['Key', 'Age']]
 
 # tkr and fl are global values. I can drive out of the five CSR files with those values.
 
@@ -135,9 +134,8 @@ for tkr in TickerList:
     for fy in range(StartYear, EndYear + 1):
         fl = fisc_list_fxn(fy, TickerOffsetDic[tkr])
 #        print (tkr, fy, TickerOffsetDic[tkr], ':', fl)
-        ta = fy - TickerFoundedDic[tkr]
         key_string = tkr + "-" + str(fy)
-        age_out_list.append([key_string, ta])
+#        age_out_list.append([key_string, ta])
 
         esg_foo2 = esg_df[esg_df.Month.isin(fl)]
         env_foo2 = env_df[env_df.Month.isin(fl)]
@@ -163,13 +161,13 @@ for tkr in TickerList:
         emp_out_list.append([key_string, emp_mf])
         gov_out_list.append([key_string, gov_mf])
 
-        print(tkr, fy, TickerOffsetDic[tkr], fl, esg_mf, ta)
-        print(tkr, fy, TickerOffsetDic[tkr], fl, env_mf, ta)
-        print(tkr, fy, TickerOffsetDic[tkr], fl, com_mf, ta)
-        print(tkr, fy, TickerOffsetDic[tkr], fl, emp_mf, ta)
-        print(tkr, fy, TickerOffsetDic[tkr], fl, gov_mf, ta)
+        print(tkr, fy, TickerOffsetDic[tkr], fl, esg_mf)
+        print(tkr, fy, TickerOffsetDic[tkr], fl, env_mf)
+        print(tkr, fy, TickerOffsetDic[tkr], fl, com_mf)
+        print(tkr, fy, TickerOffsetDic[tkr], fl, emp_mf)
+        print(tkr, fy, TickerOffsetDic[tkr], fl, gov_mf)
 
-print(age_out_list)
+#print(age_out_list)
 print (esg_out_list)
 print (env_out_list)
 print (com_out_list)
@@ -181,21 +179,21 @@ env_df2 = pd.DataFrame(env_out_list).T.set_index(0).T
 com_df2 = pd.DataFrame(com_out_list).T.set_index(0).T
 emp_df2 = pd.DataFrame(emp_out_list).T.set_index(0).T
 gov_df2 = pd.DataFrame(gov_out_list).T.set_index(0).T
-age_df2 = pd.DataFrame(age_out_list).T.set_index(0).T
+#age_df2 = pd.DataFrame(age_out_list).T.set_index(0).T
 
 print (esg_df2)
 print (env_df2)
 print (com_df2)
 print (emp_df2)
 print (gov_df2)
-print (age_df2)
+#print (age_df2)
 
 # Just start stacking together the values into a single dataframe
 out_df = pd.merge(esg_df2, env_df2, how = 'outer')
 out_df = pd.merge(out_df, com_df2, how = 'outer')
 out_df = pd.merge(out_df, emp_df2, how = 'outer')
 out_df = pd.merge(out_df, gov_df2, how = 'outer')
-out_df = pd.merge(out_df, age_df2, how = 'outer')
+#out_df = pd.merge(out_df, age_df2, how = 'outer')
 
 print (out_df)
-out_df.to_csv(OutFile)
+out_df.to_csv(out_file)
